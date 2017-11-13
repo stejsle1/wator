086c75af986acf8b7399a8fe28300c2f3a4ec0c7
stejsle1 <stejsle1@fit.cvut.cz>
2017-11-13 14:55:45 +0100

    mensi uprava kodu

--- wator/wator.py
+++ wator/wator.py
@@ -4,9 +4,6 @@ import random
 import sys
 
 class WaTor():
-   global energies
-   global creatures
-      
    def __init__(self, creatures = None, energies = None, shape = None, nfish = None, nsharks = None, age_fish=5, age_shark=10, energy_eat=3, energy_initial=0): 
       
       ok = 0
@@ -16,20 +13,22 @@ class WaTor():
       else:
          ok = 0
          creatures = numpy.zeros(shape)
+         size0 = creatures.shape[0]
+         size1 = creatures.shape[1]
             
-         if nfish == None or nsharks == None or nfish > creatures.shape[0]*creatures.shape[1] or nsharks > creatures.shape[0]*creatures.shape[1]:  
+         if nfish == None or nsharks == None or nfish > size0*size1 or nsharks > size0*size1:  
             raise ValueError("Malo parametru")
          while ok < nfish:
-            ran1 = random.randint(0, creatures.shape[0]-1) 
-            ran2 = random.randint(0, creatures.shape[1]-1) 
+            ran1 = random.randint(0, size0-1) 
+            ran2 = random.randint(0, size1-1) 
             if creatures[ran1,ran2] == 0: 
                creatures[ran1,ran2] = random.randint(1, age_fish)
                ok += 1
                
          ok = 0      
          while ok < nsharks:
-            ran1 = random.randint(0, creatures.shape[0]-1) 
-            ran2 = random.randint(0, creatures.shape[1]-1) 
+            ran1 = random.randint(0, size0-1) 
+            ran2 = random.randint(0, size1-1) 
             if creatures[ran1,ran2] == 0: 
                creatures[ran1,ran2] = -1*random.randint(1, age_shark)
                ok += 1      
@@ -62,19 +61,23 @@ class WaTor():
       creatures2 = creatures.copy()
       value = 0
       
+      size0 = creatures.shape[0]
+      size1 = creatures.shape[1]
+      
       # Fish
       for i in range(0, creatures.shape[0]):
          for j in range(0, creatures.shape[1]):
             if creatures[i,j] <= 0:
                continue
+            
             ways = 0   
-            if creatures2[(i+1)%creatures.shape[0],j] == 0:
+            if creatures2[(i+1)%size0,j] == 0:
                ways += 1
-            if creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] == 0:
+            if creatures2[(i-1)%size0,j] == 0:
                ways += 1
-            if creatures2[i,(j+1)%creatures.shape[1]] == 0:
+            if creatures2[i,(j+1)%size1] == 0:
                ways += 1
-            if creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] == 0:
+            if creatures2[i,(j-1)%size1] == 0:
                ways += 1
                
             if ways != 0:
@@ -84,17 +87,17 @@ class WaTor():
                   value = creatures[i,j] 
                while True:
                   ran = random.randint(1, 4) 
-                  if ran == 1 and creatures2[(i+1)%creatures.shape[0],j] == 0:
-                     creatures2[(i+1)%creatures.shape[0],j] = value + 1  
+                  if ran == 1 and creatures2[(i+1)%size0,j] == 0:
+                     creatures2[(i+1)%size0,j] = value + 1  
                      break
-                  if ran == 2 and creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] == 0:
-                     creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] = value + 1
+                  if ran == 2 and creatures2[(i-1)%size0,j] == 0:
+                     creatures2[(i-1)%size0,j] = value + 1
                      break
-                  if ran == 3 and creatures2[i,(j+1)%creatures.shape[1]] == 0:
-                     creatures2[i,(j+1)%creatures.shape[1]] = value + 1   
+                  if ran == 3 and creatures2[i,(j+1)%size1] == 0:
+                     creatures2[i,(j+1)%size1] = value + 1   
                      break
-                  if ran == 4 and creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] == 0:
-                     creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] = value + 1
+                  if ran == 4 and creatures2[i,(j-1)%size1] == 0:
+                     creatures2[i,(j-1)%size1] = value + 1
                      break
                if creatures[i,j] == self.age_fish:
                   creatures2[i,j] = 1
@@ -116,13 +119,13 @@ class WaTor():
             if creatures[i,j] >= 0:  
                continue
             ways = 0   
-            if creatures2[(i+1)%creatures.shape[0],j] > 0:
+            if creatures2[(i+1)%size0,j] > 0:
                ways += 1
-            if creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] > 0:
+            if creatures2[(i-1)%size0,j] > 0:
                ways += 1
-            if creatures2[i,(j+1)%creatures.shape[1]] > 0:
+            if creatures2[i,(j+1)%size1] > 0:
                ways += 1
-            if creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] > 0:
+            if creatures2[i,(j-1)%size1] > 0:
                ways += 1
                
             if ways != 0:  # je tam ryba
@@ -135,31 +138,31 @@ class WaTor():
                   creatures2[i,j] = 0      
                while True:
                   ran = random.randint(1, 4) 
-                  if ran == 1 and creatures2[(i+1)%creatures.shape[0],j] > 0:
-                     creatures2[(i+1)%creatures.shape[0],j] = value - 1
-                     energies2[(i+1)%creatures.shape[0],j] = energies[i,j] + self.eat
+                  if ran == 1 and creatures2[(i+1)%size0,j] > 0:
+                     creatures2[(i+1)%size0,j] = value - 1
+                     energies2[(i+1)%size0,j] = energies[i,j] + self.eat
                      break
-                  if ran == 2 and creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] > 0:
-                     creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] = value - 1                                                                               
-                     energies2[(i-1+creatures.shape[0])%creatures.shape[0],j] = energies[i,j] + self.eat
+                  if ran == 2 and creatures2[(i-1)%size0,j] > 0:
+                     creatures2[(i-1)%size0,j] = value - 1                                                                               
+                     energies2[(i-1)%size0,j] = energies[i,j] + self.eat
                      break
-                  if ran == 3 and creatures2[i,(j+1)%creatures.shape[1]] > 0:
-                     creatures2[i,(j+1)%creatures.shape[1]] = value - 1
-                     energies2[i,(j+1)%creatures.shape[1]] = energies[i,j] + self.eat
+                  if ran == 3 and creatures2[i,(j+1)%size1] > 0:
+                     creatures2[i,(j+1)%size1] = value - 1
+                     energies2[i,(j+1)%size1] = energies[i,j] + self.eat
                      break
-                  if ran == 4 and creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] > 0:
-                     creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] = value - 1
-                     energies2[i,(j-1+creatures.shape[1])%creatures.shape[1]] = energies[i,j] + self.eat
+                  if ran == 4 and creatures2[i,(j-1)%size1] > 0:
+                     creatures2[i,(j-1)%size1] = value - 1
+                     energies2[i,(j-1)%size1] = energies[i,j] + self.eat
                      break
             else:      # neni tam ryba
                ways = 0   
-               if creatures2[(i+1)%creatures.shape[0],j] == 0:
+               if creatures2[(i+1)%size0,j] == 0:
                   ways += 1 
-               if creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] == 0:
+               if creatures2[(i-1)%size0,j] == 0:
                   ways += 1
-               if creatures2[i,(j+1)%creatures.shape[1]] == 0:
+               if creatures2[i,(j+1)%size1] == 0:
                   ways += 1
-               if creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] == 0:
+               if creatures2[i,(j-1)%size1] == 0:
                   ways += 1
                
                if ways != 0:
@@ -172,21 +175,21 @@ class WaTor():
                      creatures2[i,j] = 0  
                   while True:
                      ran = random.randint(1, 4) 
-                     if ran == 1 and creatures[(i+1)%creatures.shape[0],j] == 0:
-                        creatures2[(i+1)%creatures.shape[0],j] = value - 1
-                        energies2[(i+1)%creatures.shape[0],j] = energies[i,j]
+                     if ran == 1 and creatures[(i+1)%size0,j] == 0:
+                        creatures2[(i+1)%size0,j] = value - 1
+                        energies2[(i+1)%size0,j] = energies[i,j]
                         break
-                     if ran == 2 and creatures[(i-1+creatures.shape[0])%creatures.shape[0],j] == 0:
-                        creatures2[(i-1+creatures.shape[0])%creatures.shape[0],j] = value - 1                                
-                        energies2[(i-1+creatures.shape[0])%creatures.shape[0],j] = energies[i,j]
+                     if ran == 2 and creatures[(i-1)%size0,j] == 0:
+                        creatures2[(i-1)%size0,j] = value - 1                                
+                        energies2[(i-1)%size0,j] = energies[i,j]
                         break
-                     if ran == 3 and creatures[i,(j+1)%creatures.shape[1]] == 0:
-                        creatures2[i,(j+1)%creatures.shape[1]] = value - 1 
-                        energies2[i,(j+1)%creatures.shape[1]] = energies[i,j]
+                     if ran == 3 and creatures[i,(j+1)%size1] == 0:
+                        creatures2[i,(j+1)%size1] = value - 1 
+                        energies2[i,(j+1)%size1] = energies[i,j]
                         break
-                     if ran == 4 and creatures[i,(j-1+creatures.shape[1])%creatures.shape[1]] == 0:
-                        creatures2[i,(j-1+creatures.shape[1])%creatures.shape[1]] = value - 1                                                                   
-                        energies2[i,(j-1+creatures.shape[1])%creatures.shape[1]] = energies[i,j]
+                     if ran == 4 and creatures[i,(j-1)%size1] == 0:
+                        creatures2[i,(j-1)%size1] = value - 1                                                                   
+                        energies2[i,(j-1)%size1] = energies[i,j]
                         break
                else:  # neni misto
                   if creatures[i,j] == -1*self.age_shark:
@@ -208,7 +211,7 @@ class WaTor():
       self.energies = energies
       
       return self 
-      
+       
                   
    def count_fish(self):
       boolarr = self.creatures > 0
