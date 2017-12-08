a3e42ab7beb97bef50c427e5fb9b2c12b527a5fe
stejsle1 <stejsle1@fit.cvut.cz>
2017-12-08 09:17:34 +0100

    gui - uprava setup.py kvuli nove koncovce README.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
-with open('README') as f:
+with open('README.rst') as f:
     long_description = ''.join(f.readlines())
 
 
